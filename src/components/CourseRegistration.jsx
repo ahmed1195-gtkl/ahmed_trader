fe5cf2133933f3245ca576c9da69c8f10896b721
ahmed1195-gtkl <mchokri100@gmail.COM>
2026-01-26 02:07:49 +0100

    Update CourseRegistration.jsx

--- src/components/CourseRegistration.jsx
+++ src/components/CourseRegistration.jsx
@@ -101,7 +101,7 @@ const CourseRegistration = () => {
     params.append('date', new Date().toLocaleString('en-GB', { timeZone: 'UTC' }));
 
     try {
-      const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbyqz3Xvm-N55dN48bDW57TCuKZnvV66VBAOwJ_iDM4wMu57nn2T30Am1sQ8C8c-cjPUBw/exec';
+      const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbwujUQoa2D-_s_GrDail2lq0T0F-CaxcofH33cQUAxKoB3Kv6DKXTclN9bmxqBF1291XQ/exec';
       
       // Sending as application/x-www-form-urlencoded via URLSearchParams
       await fetch(GOOGLE_SHEET_URL, {
